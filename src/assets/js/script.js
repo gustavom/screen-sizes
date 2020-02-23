5311fd692da0faa113158b47b9c85b6b080bc026
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-23 00:47:52 -0300

    init app js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,15 @@
 (function(window, document) {
   "use strict";
 
+  var $form = document.querySelector(".project-form form");
+  var $urlField = document.querySelector(".project-form form input");
+  var $ScreenFrames = document.querySelectorAll(".screens-container iframe");
+
+  $form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    console.log($urlField.value);
+    Array.prototype.forEach.call($ScreenFrames, function(item) {
+      item.setAttribute("src", "http://www.google.com");
+    });
+  });
 })(window, document);
